fedace3a6f299fc8839ac0f5133d06add7400306
Lord-Chalex <146172598+Lord-Chalex@users.noreply.github.com>
2024-03-12 13:01:00 +0000

    v1

--- ap.py
+++ ap.py
@@ -1,1 +1,9 @@
+def maxliste(liste):
+    max_val = 0
+    for x in range (len(liste)):
+        if liste [x] > max_val :
+            max_val = liste[x]
+        return max_val
 
+assert maxliste([98,12,104,23,131,9])
+assert maxliste([-27,24,-3,15])
